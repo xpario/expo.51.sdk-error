5054fbaae80d51ecc81d74b6495135762425647f
YachtWave <131918720+yachtwave@users.noreply.github.com>
2024-05-21 20:57:35 -0400

    UPDATED 05-21-2024-C

--- App.js
+++ App.js
@@ -8,15 +8,29 @@ async function initializeFonts() {
 	try {
 
 		await loadAsync({
+			/*
+			'Inter_100Thin': require('./assets/inter/Inter-Thin.ttf'),
+			//'Inter_200ExtraLight': require('./assets/manrope/Manrope-ExtraLight.ttf'),
+			//'Inter_300Light': require('./assets/manrope/Manrope-Light.ttf'),
+			'Inter_400Regular': require('./assets/manrope/Manrope-Regular.ttf'),
+			//'Inter_500Medium': require('./assets/manrope/Manrope-Medium.ttf'),
+			'Inter_600SemiBold': require('./assets/manrope/Manrope-SemiBold.ttf'),
+			'Inter_700Bold': require('./assets/manrope/Manrope-Bold.ttf'),
+			'Inter_800ExtraBold': require('./assets/manrope/Manrope-ExtraBold.ttf'),
+			'Inter_900Black': require('./assets/inter/Inter-Black.ttf'),
+			 */
+
 			'Inter_100Thin': require('./assets/inter/Inter-Thin.ttf'),
 			'Inter_200ExtraLight': require('./assets/inter/Inter-ExtraLight.ttf'),
 			'Inter_300Light': require('./assets/inter/Inter-Light.ttf'),
 			'Inter_400Regular': require('./assets/inter/Inter-Regular.ttf'),
 			'Inter_500Medium': require('./assets/inter/Inter-Medium.ttf'),
-			'Inter_600SemiBold': require('./assets/inter/Inter-SemiBold.ttf'),
+			//'Inter_600SemiBold': require('./assets/inter/Inter-SemiBold.ttf'),
+			'Inter_600SemiBold': require('./assets/manrope/Manrope-SemiBold.ttf'),
 			'Inter_700Bold': require('./assets/inter/Inter-Bold.ttf'),
 			'Inter_800ExtraBold': require('./assets/inter/Inter-ExtraBold.ttf'),
 			'Inter_900Black': require('./assets/inter/Inter-Black.ttf'),
+
 		});
 		// Below is for testing purposes only...
 		// throw new Error('Test environment exception.');
@@ -34,13 +48,20 @@ async function initializeFonts() {
 
 const App = () => {
 
+	const [fontsLoaded, setFontsLoaded] = React.useState(false);
+
 	initializeFonts().then((loaded) => {
 		if (loaded) {
+			setFontsLoaded(true);
 			console.log('Fonts loaded successfully.');
 		} else {
 			console.log('Fonts failed to load.');
 		}
 	});
+
+	if (!fontsLoaded) {
+		return null;
+	}
 
 	return (
 		<View
